81794de8bf92af5cb379bcbfc6a0a8e90323a1b1
MarcoSapone <145933007+MarcoSapone@users.noreply.github.com>
2025-05-05 09:59:56 +0000

    code changed

--- src/tests_vari/test_file.cpp
+++ src/tests_vari/test_file.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+#include <cstdint>
+
+template <typename T>
+int size_in_bits(T a) {
+    return sizeof(a) *8;
+}
+
+
+int main(){
+
+      std::cout << "The size in bit is:" << size_in_bits(21) << std::endl;
+    std::cout << "The size in bit is:" << size_in_bits('f') << std::endl;
+    std::cout << "The size in bit is:" << size_in_bits(32.1f) << std::endl;
+    std::cout << "The size in bit is:" << size_in_bits(32.1) << std::endl;
+    std::cout << std::endl << std::endl;
+    return 0;
+}
